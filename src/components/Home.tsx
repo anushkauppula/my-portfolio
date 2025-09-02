98107a61a386c6d00a67155855866c194ba4ee03
Anushka Uppula <ANUSHKACHARY@GMAIL.COM>
2025-09-01 21:31:41 -0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,14 +4,21 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col sm:flex-row">
       {/* Left: Full image */}
-      <div className="sm:w-1/2 w-full flex items-center justify-center bg-slate-100" style={{ minHeight: '300px' }}>
-        <div className="flex items-center justify-center w-full h-full py-8 sm:py-0">
+      <div className="sm:w-1/2 w-full min-h-screen sm:h-screen flex items-center justify-center bg-slate-100">
+        {/* Mobile view: vertically centered image */}
+        <div className="flex flex-col justify-center items-center w-full h-full sm:hidden min-h-screen">
           <img
             src={require('../photo.jpeg')}
             alt="Profile"
             className="w-full h-auto max-h-[350px] object-cover object-center rounded-xl shadow"
           />
         </div>
+        {/* Desktop/web view: full height image */}
+        <img
+          src={require('../photo.jpeg')}
+          alt="Profile"
+          className="hidden sm:block w-full h-full object-cover object-center"
+        />
       </div>
       {/* Right: Centered text */}
       <div className="sm:w-1/2 w-full flex flex-col items-center justify-center px-4 py-10 sm:px-6 sm:py-0 bg-white">
